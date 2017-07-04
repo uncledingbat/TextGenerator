99c8a432dd4684098028e33f34ed88889d9cd4d4
Haoyou Liu <liuhaoyou1222@gmail.com>
2017-07-04 15:48:59 +0800

    Update train.py

--- train.py
+++ train.py
@@ -18,24 +18,25 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # =============================================================================
 
 # Data parameters
-tf.flags.DEFINE_string('data_file', 'test.csv', 'Data file path')
+tf.flags.DEFINE_string('data_file', '1.csv', 'Data file path')
 tf.flags.DEFINE_string('stop_word_file', 'stop_words_ch.txt', 'Stop word file path')
 tf.flags.DEFINE_string('language', 'ch', "Language of the data file. You have two choices: ['ch', 'en']")
 tf.flags.DEFINE_integer('vocab_size', 0, 'Vocabulary size')
-tf.flags.DEFINE_integer('level', 1, '1 for char level, 2 for phrase level')
+tf.flags.DEFINE_integer('level', 2, '1 for char level, 2 for phrase level')
 
 # Hyperparameters
 tf.flags.DEFINE_integer('embedding_size', 128, 'Word embedding size')
 tf.flags.DEFINE_integer('hidden_size', 128, 'Number of hidden units in the LSTM cell')  # RNN
-tf.flags.DEFINE_integer('num_layers', 2, 'Number of the LSTM cells')  # RNN
+tf.flags.DEFINE_integer('num_layers', 3, 'Number of the LSTM cells')  # RNN
 tf.flags.DEFINE_integer('keep_prob', 0.5, 'Dropout keep probability')
 tf.flags.DEFINE_float('learning_rate', 1e-3, 'Learning rate')
 tf.flags.DEFINE_float('l2_reg_lambda', 0.0, 'L2 regularization lambda')
 tf.flags.DEFINE_float('grad_norm', 5.0, 'Gradient clipping')
 
 # Training parameters
-tf.flags.DEFINE_integer('batch_size', 64, 'Batch size')
+tf.flags.DEFINE_integer('batch_size', 128, 'Batch size')
 tf.flags.DEFINE_integer('num_epochs', 50, 'Number of epochs')
+tf.flags.DEFINE_integer('evaluate_every_steps', 1000, 'Evaluate the model after this many steps')
 tf.flags.DEFINE_integer('save_every_steps', 1000, 'Save the model after this many steps')
 tf.flags.DEFINE_integer('num_checkpoint', 20, 'Number of models to store')
 
@@ -116,14 +117,28 @@ with tf.Graph().as_default():
             time_str = datetime.datetime.now().isoformat()
             print("{}: step: {}, loss: {:g}".format(time_str, step, cost))
 
+            return cost
+
 
         print('Start training ...')
+        start = time.time()
 
+        total_cost = 0
         for batch in batches:
-            run_step(batch)
+            cost = run_step(batch)
+            total_cost += cost
             current_step = tf.train.global_step(sess, lm.global_step)
+
+            if current_step % FLAGS.evaluate_every_steps == 0:
+                aver_cost = total_cost / FLAGS.evaluate_every_steps
+                print('\nAverage cost at step {}: {}'.format(current_step, aver_cost))
+                total_cost = 0
 
             if current_step % FLAGS.save_every_steps == 0:
                 save_path = saver.save(sess, os.path.join(outdir, 'model/lm'), current_step)
+                print('\nModel saved to {}\n'.format(save_path))
 
-        print('\nAll the files have been saved to {}\n'.format(outdir))
+        end = time.time()
+
+        print(('\nRun time: {}'.format(end - start)))
+        print('\nAll the files have been saved to {}'.format(outdir))
